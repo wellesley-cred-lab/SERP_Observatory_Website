28b6fecf1fad411410c7a5f3403a436d5c2edfcf
Amy Hu <amyhu@Amys-MacBook-Air.local>
2022-07-01 11:43:56 -0400

    Dynamically Render the Dates Page for each Category and Add node_modules

--- flaskr/dates.py
+++ flaskr/dates.py
@@ -7,29 +7,26 @@ from flask import (
 
 bp = Blueprint('dates', __name__)
 
-@bp.route('/dates')
-def date():
-    os.chdir(bp.root_path+"/SERP_Collection")
-    categories = os.listdir()
-    for category in categories:
-        if not category.startswith('.'):
-            dateFolders = os.listdir(category)
-            datesList = []
-            # Place the date folders in order (Most recent on top)
-            for date in dateFolders:
-                if not date.startswith('.'):
-                    dateComponents = date.split("-")
-                    datesList.append({'month':int(dateComponents[0]), 'day':int(dateComponents[1]), 'year':int(dateComponents[2])})
-                datesList = sorted(datesList, key=lambda x: (x['year'], x['month'], x['day']),reverse=True)
-                sortedDates = []
-                for d in datesList:
-                    sortedDates.append(str(d['month'])+'-'+str(d['day'])+'-'+str(d['year']))
-                dateFolders = sortedDates 
-            for date in dateFolders:
-                if not date.startswith('.'):
-                    logging.warning(date)
-            logging.warning(category)
-            logging.warning(dateFolders)
+@bp.route('/<category>')
+def date(category):
+    os.chdir(bp.root_path+f"/SERP_Collection/{category}")
+    dateFolders = os.listdir()
+    datesList = []
+    # Place the date folders in order (Most recent on top)
+    for date in dateFolders:
+        if not date.startswith('.'):
+            dateComponents = date.split("-")
+            datesList.append({'month':int(dateComponents[0]), 'day':int(dateComponents[1]), 'year':int(dateComponents[2])})
+        datesList = sorted(datesList, key=lambda x: (x['year'], x['month'], x['day']),reverse=True)
+        sortedDates = []
+        for d in datesList:
+            sortedDates.append(str(d['month'])+'-'+str(d['day'])+'-'+str(d['year']))
+        dateFolders = sortedDates 
+    for date in dateFolders:
+        if not date.startswith('.'):
+            logging.warning(date)
+    logging.warning(category)
+    logging.warning(dateFolders)
     return render_template('dates/dateList.html', title='Home', dateFolders=dateFolders)   
         
     #logging.warning(categories)
